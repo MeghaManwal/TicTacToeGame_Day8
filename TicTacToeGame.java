7e98beea3bf0656dc119d50fe40c40d26e0788fa
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-25 10:46:06 +0530

    Added:To Print the game Board to Play

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -29,12 +29,26 @@ class TicTacToeGame {
 		       System.out.println(" The Player choose O");
 		  }
 	
+	public static void printboard() {
+		 System.out.print("--------------\n");
+		
+		  for (int i=0; i<3; i++) {
+			System.out.print("|");
+			for (int j=0; j<3; j++) {
+				System.out.print(board[i][j] + "|");
+			}
+			System.out.println();
+			System.out.println("--------------");
+		  }
+	}
+	
 	public static void main(String[] args) {
 	     board = new char[3][3];
 		 initializeBoard ();
 		 
 	     System.out.println("Welcome to TicTacToe Game");
 		 choice ();
+		 printboard();
 	}
 }
 
