13aa5338a5b011342f77a29931059f36a8117390
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-04-25 10:36:44 +0530

    Added:To choose the symbol by the Player to Play

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package ticTacToeGame;
 
+import java.util.Scanner;
+
 class TicTacToeGame {
 	
 	private static char  [][] board;
@@ -7,17 +9,32 @@ class TicTacToeGame {
 	
 	
 	public static void initializeBoard() {
-		for (int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
+		for (int i=0;i<3;i++) 
+		{
+			for(int j=0;j<3;j++) 
+			{
 				board[i][j]='-';
 			}
 		}
 	}
+	
+	public static void choice() {
+	     Scanner s=new Scanner(System.in);
+	     System.out.println(" Enter 1 to choose X\n Enter 2 to choose O");
+
+		 int n=s.nextInt();
+		     if( n==1)
+		       System.out.println(" The Player choose X");
+		     else
+		       System.out.println(" The Player choose O");
+		  }
+	
 	public static void main(String[] args) {
-		board = new char[3][3];
+	     board = new char[3][3];
 		 initializeBoard ();
 		 
-		 System.out.println("Welcome to TicTacToe Game");
+	     System.out.println("Welcome to TicTacToe Game");
+		 choice ();
 	}
 }
 
